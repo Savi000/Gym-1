9ab3bf89a027046efe9d305ff55d98c970140e84
Filip S <sawicewic1@gmail.com>
2021-12-18 15:46:26 +0100

    ready

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 module.exports = {
   reactStrictMode: true,
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          ...
-          {
-            loader: 'css-loader',
-          }, {
-            loader: 'resolve-url-loader',
-          }, {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              sourceMapContents: false
-            }
+  rules: [
+    {
+      test: /\.scss$/,
+      use: [
+        ...
+        {
+          loader: 'css-loader',
+        }, {
+          loader: 'resolve-url-loader',
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            sourceMapContents: false
           }
-        ]
-      },
-    ]
-  }
+        }
+      ]
+    }
+  ]
+
 
 }
 
